788543586cd76571fdf50196482845edbb617da6
Ramesh1402 <reachrameshmobiledev@gmail.com>
2024-05-25 15:40:16 +0530

    Initial Commit

---
+++ LOTRConverter91/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LOTRConverter91
+//
+//  Created by Ramesh Shanmugam on 25/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTRConverter91/LOTRConverter91App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter91App.swift
+//  LOTRConverter91
+//
+//  Created by Ramesh Shanmugam on 25/05/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter91App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
