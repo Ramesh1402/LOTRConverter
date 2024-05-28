24b1a1f7056a1dfbfbe695db305c24590f229a20
Ramesh1402 <reachrameshmobiledev@gmail.com>
2024-05-28 16:07:52 +0530

    feat: design first version of main page

--- LOTRConverter91/ContentView.swift
+++ LOTRConverter91/ContentView.swift
@@ -18,8 +18,15 @@ struct ContentView: View {
             
             VStack {
                 //Pony Image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
                 
-                //Currency Text
+                //Currency Exchange Text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
                 
                 //Currency Conversion
                 HStack {
@@ -28,29 +35,54 @@ struct ContentView: View {
                         //Currency
                         HStack {
                             //CurrencyImage
+                            Image(.silverpenny)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                             
                             //Currency Text
+                            Text("Silver Penny")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                         }
                         
                         //TextField
+                        Text("Text Field")
                     }
                     
                     //Equal sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
                     
                     //Right Currency Section
                     VStack {
                         //Currency
                         HStack {
                             //Currency Text
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                             
                             //CurrencyImage
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                         }
                         
                         //TextField
+                        Text("Text Field")
                     }
                 }
                 
+                Spacer()
+                
                 //Info Button
+                Image(systemName: "info.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
             }
         }
     }
