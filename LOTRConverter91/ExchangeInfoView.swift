acfee1cd3295f8392f4cfcfb111a72d55ee9c45b
Ramesh1402 <reachrameshmobiledev@gmail.com>
2024-06-08 21:10:34 +0530

    feat: add exchange info view

---
+++ LOTRConverter91/ExchangeInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  ExchangeInfoView.swift
+//  LOTRConverter91
+//
+//  Created by Ramesh Shanmugam on 08/06/24.
+//
+
+import SwiftUI
+
+struct ExchangeInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExchangeInfoView()
+}
