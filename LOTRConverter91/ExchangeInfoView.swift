9a00b30045855283c842f9fbc4e5fae9728019de
Ramesh1402 <reachrameshmobiledev@gmail.com>
2024-06-15 20:51:45 +0530

    feat: add exchange info layout

--- LOTRConverter91/ExchangeInfoView.swift
+++ LOTRConverter91/ExchangeInfoView.swift
@@ -9,7 +9,55 @@ import SwiftUI
 
 struct ExchangeInfoView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            //Background Image
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            
+            VStack {
+                //Title
+                Text("Exchange Rates")
+                    .font(.largeTitle)
+                    .tracking(3)
+                
+                //Description
+                Text("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world except one. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Below is a simple guide to our currency exchange rates:")
+                    .font(.title2)
+                    .padding()
+                
+                //Exchange text
+                HStack {
+                    //Left Currency
+                    Image(.goldpiece)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33)
+                    
+                    
+                    //Exchange Rate
+                    Text("1 Gold Piece = 4 Gold Pennies")
+                    
+                    //Right Currency
+                    Image(.goldpenny)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33)
+                }
+                
+                //Done Button
+                Button("Done") {
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown)
+                .foregroundStyle(.white)
+                .font(.largeTitle)
+                .padding()
+            }
+            .foregroundStyle(.black)
+        }
     }
 }
 
