15a367fd9c3efe8fc0c48cc744f336ea5654411e
Ramesh1402 <reachrameshmobiledev@gmail.com>
2024-07-21 18:54:10 +0530

    feat: add currency data model

---
+++ LOTRConverter91/Model/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  LOTRConverter91
+//
+//  Created by Ramesh Shanmugam on 21/07/24.
+//
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+}
